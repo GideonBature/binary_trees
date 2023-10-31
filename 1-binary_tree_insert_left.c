757df7469665ac9ad3fcc086ddbd3d15ec611f1c
GideonBature <infoaboutgideon@gmail.com>
2023-10-31 19:46:05 +0000

    fix code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,18 +19,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	new_node->parent = parent;;
-	new_node->right = NULL;
-	new_node->n = value;
-
 	if (parent->left)
-	{
-		new_node->left = parent->left;
 		parent->left->parent = new_node;
-	}
-	else
-		new_node->parent = NULL;
 
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = parent->left;
+	new_node->right = NULL;
 	parent->left = new_node;
 
 	return (new_node);
