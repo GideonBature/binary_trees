bbbc27f53051026704ed5502e99a59a31e677e56
GideonBature <infoaboutgideon@gmail.com>
2023-10-31 19:58:19 +0000

    fix code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,14 +8,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (!node)
 		return (0);
 
 	if (!node->left && !node->right)
 		return (1);
-
-	if ((node->left) && (node->right))
-		return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
-
-	return (0);
+	else
+		return (0);
 }
