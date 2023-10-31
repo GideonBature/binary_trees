332b28ff40bb6c2ab1e0f9cb4dc5498debe49973
GideonBature <infoaboutgideon@gmail.com>
2023-10-31 17:53:09 +0000

    fix code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->n = value;
+	new_node->parent = parent;
 
 	if (parent)
 	{
