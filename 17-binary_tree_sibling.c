84594c3dee335afb5053971ef29078ffd81e1a9a
GideonBature <infoaboutgideon@gmail.com>
2023-11-01 19:24:46 +0000

    optimized code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (!node->parent->left)
-		return (NULL);
-	else if (!node->parent->right)
+	if (!node->parent->left || !node->parent->right)
 		return (NULL);
 	else if (node->parent->left == node)
 		return (node->parent->right);
